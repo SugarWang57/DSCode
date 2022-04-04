5d0b7e20375c2df826acdca32eeb171aebad6afb
Sugar MacBook Pro <2247945677@qq.com>
2022-04-04 20:09:10 +0800

    更新了顺序表的代码实现

---
+++ Chapter II/SqList.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define elemType int
+#define INITSIZE 10
+#define INCREASESIZE 10
+
+typedef struct {
+    elemType* data;
+    int length;
+    int maxSize;
+}sqList;
+
+bool increaseMemory(sqList &L){
+    sqList T;
+    T.data = (elemType*)malloc(sizeof(elemType) * (L.maxSize + INCREASESIZE));
+    T.data = L.data;
+    L = T;
+    return true;
+}
+
+bool initSqList(sqList &L){
+    if(!(L.data = (elemType*)malloc(sizeof(elemType) * INITSIZE)))
+        return false;
+    L.length = 0;
+    L.maxSize = INITSIZE;
+    return true;
+}
+
+bool insert(sqList &L,int index,int e){
+    if(L.length == L.maxSize)
+        increaseMemory(L);
+    if(index <= 0 || index > L.maxSize)
+        return false;
+       
+    if(index <= L.length + 1){
+        elemType t = e;
+
+    for(int a = L.length;a >= index;a--){
+        L.data[a] = L.data[a - 1];
+    }
+    L.data[index - 1] = t;
+
+    L.length++;
+    printf("插入成功！\n");
+        return true;
+    }
+    
+    if(index > L.length + 1){
+        printf("插入位置错误！\n");
+        return false;
+    }
+    
+}
+
+elemType deleteElem(sqList &L,int index){
+    if(index <= 0 || index > L.length)
+        return false;;
+    int t = L.data[index - 1];
+    for(int a = index;a <= L.length;a++){
+        L.data[a - 1] = L.data[a];
+    }
+    L.length--;
+    printf("删除成功！\n");
+    return t;
+    
+}
+
+void traverse(sqList L){
+    if(L.length == 0){
+        printf("表为空!\n");
+        return;
+    }
+
+    printf("开始遍历: ");
+    for(int i = 1; i <= L.length;i++){
+        printf("%d ",L.data[i - 1]);
+    }   
+    printf("\n");
+}
+
+int length(sqList L){
+    return L.length;
+}
+
+elemType searchByIndex(sqList L,int index){
+    if(index >= 0 && index <= L.length && L.length != 0)
+        return L.data[index - 1];
+}
+
+void searchByElem(sqList L,elemType elem){ //输出所有遇到的elem
+    printf("searching by Elem …… \n");
+    if(L.length != 0){
+        for(int i = 1;i <= L.length;i++){
+            if(L.data[i - 1] == elem){
+                printf("%d位置为元素%d\n",i,elem);
+            }
+        }
+    }
+}
+
+bool clearSqList(sqList &L){
+    L.data = NULL;
+    L.length = 0;
+    printf("清空表成功！\n");
+    return true;
+}
+
+bool destroySqList(sqList &L){
+    L.data = NULL;
+    L.length = 0;
+    free(L.data);
+    printf("销毁表成功！\n");
+    return true;
+        
+}
+
+int main(){
+    sqList L;
+    if(initSqList(L))
+        printf("初始化成功！\n");
+    insert(L,1,2);
+    insert(L,2,3);
+    insert(L,3,4);
+    insert(L,4,5);
+    insert(L,2,6);
+    traverse(L);
+    deleteElem(L,2);
+    insert(L,6,3);
+    traverse(L);
+    printf("%d位置调用search方法返回： %d\n",3,searchByIndex(L,3));
+    searchByElem(L,3);
+    clearSqList(L);
+    traverse(L);
+    destroySqList(L);
+    return 0;
+}
